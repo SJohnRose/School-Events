a453c903332816c08a35170bb510ceccf8854685
SJohnRose <shamini.johnrose@gmail.com>
2023-06-28 21:50:17 +1000

    implemented getEvents method to fetch events

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,7 +1,16 @@
 //Create handlers for the routes
+import EventMessage from '../models/eventMessage.js';
 
-export const getEvents = (req, res) => {
-    res.send('Test Route!');
+export const getEvents = async (req, res) => {
+    try {
+        const eventMessages =  await EventMessage.find();  
+        console.log(eventMessages);
+        res.status(200).json(eventMessages);
+    }
+    catch(error) {
+        res.status(404).json({message: error.message});
+    }
+    //res.send('Test Route!');
 };
 
 export const createEvent = (req, res) => {
