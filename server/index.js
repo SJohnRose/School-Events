c49be0feb76bf376597219d1da93a9cedd024c3d
SJohnRose <shamini.johnrose@gmail.com>
2023-06-28 21:16:50 +1000

    changed mongodb credentials

--- server/index.js
+++ server/index.js
@@ -2,20 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 // To send large images
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-// To get database hosted in Mongo Atlas
-const CONNECTION_URL = 'mongodb+srv://shaminijohnrose:gYP87xgn3EbP6G39@eventcluster.pcnkj2q.mongodb.net/?retryWrites=true&w=majority';
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then( () => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch( (error) => console.log(error.message));
 
