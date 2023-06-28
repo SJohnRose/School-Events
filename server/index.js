00d3ff5b5373188d77eeeb2de71c0cf0053a8d88
SJohnRose <shamini.johnrose@gmail.com>
2023-06-28 21:06:00 +1000

    Updated server/index.js with connection string for Mongo Atlas db

--- server/index.js
+++ server/index.js
@@ -3,3 +3,20 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express();
+
+// To send large images
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+// To get database hosted in Mongo Atlas
+const CONNECTION_URL = 'mongodb+srv://shaminijohnrose:gYP87xgn3EbP6G39@eventcluster.pcnkj2q.mongodb.net/?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch( (error) => console.log(error.message));
+
+
