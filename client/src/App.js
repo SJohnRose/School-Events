b2035af115b897193ee1333bef1b6a4892da4e33
SJohnRose <shamini.johnrose@gmail.com>
2023-07-10 20:59:35 +1000

    added Grid components to App.js to show SchoolEvents and Form

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,18 @@ function App() {
         <Typography variant='h2' align='center'> School Events </Typography>
         <img src={schoolevents} alt='schoolevents' height='60'/>
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <SchoolEvents />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
